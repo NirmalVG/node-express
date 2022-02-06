0e5ad935ddb4f92055f7ef45d3e0eaa372a6ba36
Nirmal V G <nirmalvg97@gmail.com>
2022-02-06 19:06:18 +0530

    Assignment 1

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const http = require("http");
+http = require("http");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
 const dishRouter = require("./routes/dishRouter");
-
+const promoRouter = require("./routes/promoRouter");
+const leaderRouter = require("./routes/leaderRouter");
 const hostname = "localhost";
 const port = 3000;
 
@@ -13,6 +14,8 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 app.use(express.static(__dirname + "/public"));
 
 app.use((req, res, next) => {
